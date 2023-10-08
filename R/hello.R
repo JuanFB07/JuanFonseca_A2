9b4d324c1d4bede3c7aad87df04de3618829e748
JuanFB07 <juanfon9@gmail.com>
2023-10-08 18:38:43 -0500

    .

--- R/hello.R
+++ R/hello.R
@@ -27,26 +27,37 @@ pairs.panels(data1 [c("Age", "BMI", "Education", "GenHlth")],
 
 ## selection of 1500 samples of each factor of the dataset
 set.seed(1)
-data_estratificada <- data %>%
+data_est <- data %>%
   group_by(Diabetes_012) %>%
   sample_n(1500, replace = TRUE) %>%
   ungroup()
 
 
-sample.index <- sample(1:nrow(data_estratificada)
-                       ,nrow(data_estratificada)*0.7
+sample.index <- sample(1:nrow(data_est)
+                       ,nrow(data_est)*0.7
                        ,replace = F)
 
 
 predictors <- c("HighBP", "HighChol", "CholCheck", "BMI", "Smoker", "Stroke", "HeartDiseaseorAttack", "PhysActivity", "Fruits", "Veggies", "HvyAlcoholConsump", "AnyHealthcare", "NoDocbcCost", "GenHlth", "MentHlth", "PhysHlth", "DiffWalk", "Sex", "Age", "Education", "Income")
 
 # Original data
-train.data <- data_estratificada[sample.index, c(predictors, "Diabetes_012"), drop = FALSE]
-test.data <- data_estratificada[-sample.index, c(predictors, "Diabetes_012"), drop = FALSE]
+train.data <- data_est[sample.index, c(predictors, "Diabetes_012"), drop = FALSE]
+test.data <- data_est[-sample.index, c(predictors, "Diabetes_012"), drop = FALSE]
 
 
 train.data$Diabetes_012 <- factor(train.data$Diabetes_012)
 test.data$Diabetes_012 <- factor(test.data$Diabetes_012)
 
+train.data$Diabetes_012 <- factor(train.data$Diabetes_012)
+test.data$Diabetes_012 <- factor(test.data$Diabetes_012)
 
+# Train the k-NN model
+ctrl <- trainControl(method = "cv", p = 0.7)
+knnFit <- train(Diabetes_012 ~ .
+                , data = train.data
+                , method = "knn", trControl = ctrl
+                , preProcess = c("range") # c("center", "scale") for z-score
+                , tuneLength = 50)
+
+plot(knnFit)
 
